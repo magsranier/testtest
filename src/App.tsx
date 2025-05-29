40bdb2911a0941f299f5adbb322d74effa6c70f7
mags <ranier.magno@snsofttech.ph>
2025-05-29 13:34:42 +0800

    feat: 8

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import "./index.css";
 
 const App = () => {
   const btnClicked = () => {
-    const tgApp = window?.Telegram?.WebApp;
-    if (tgApp) {
-      // Add { tryBrowser: true } to the openLink options
-      tgApp.openLink("https://github.com/", { tryBrowser: true });
-    } else {
-      // Fallback for when Telegram Web App SDK is not available
-      window.open("https://github.com/", "_blank");
-    }
+    // const tgApp = window?.Telegram?.WebApp;
+    // if (tgApp) {
+    // Add { tryBrowser: true } to the openLink options
+    // tgApp.openLink("https://github.com/", { tryBrowser: true });
+    // } else {
+    // Fallback for when Telegram Web App SDK is not available
+    window.open("https://github.com/", "_blank");
+    // }
     alert("TEST");
   };
 
