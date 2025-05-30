cdfbdcd33abc44a17cc5911a5d8abe311493e15a
mags <ranier.magno@snsofttech.ph>
2025-05-30 14:02:47 +0800

    feat: change ulr

--- src/Gcash.tsx
+++ src/Gcash.tsx
@@ -13,7 +13,7 @@ const Gcash = () => {
       tgApp.openLink(
         `https://testtest-git-main-magsraniers-projects.vercel.app/gcash/?redirect_url=` +
           encodeURIComponent(
-            "https://uat.m.gcash.com/gcash-cashier-web/1.2.1/index.html#/confirm?bizNo=20250530121212800110170628202366369&timestamp=1748576054348&sign=UNDkX8WKSmda9iZ9uLyzEc%2BBw7E3T1R22bN0Ks7PH8xINcU1%2F5FeuEWMmG1ztY%2Fa%2BDv96jU4MHV%2B0gPFTNo%2FjsG21GDKxCa8pYC%2FNqRs%2BlygmlsE3Xl4KpPEv6MknzNCvJs4GjUJk5D%2BpJ3NoNepvSr0L9%2Bw4eoWLVi3GKsmoLcTDmoNsSY7Dwp0foeOU6IAJno3DS3WnnR5CfnxazrUUQhE5uQODAbRwl7U3u7S9uW9R9hPNpnIH2An4geoRlHXf0hSu11%2Feq5UWI9IrLmz5cP9l7nMLcXQacy%2B%2By9X2Uz0XwBxiNtOrgjXLnG26V13rG1miaGRg1Go%2FYDGNbENVw%3D%3D&orderAmount=20.00&pdCode=51051000101000100001&merchantVersion=v2&merchantid=217020000648378724964&queryInterval=10000&qrcode=GCSHWPV220250530121212800110170628202366369,217020000648378724964&merchantName=Stotsenberg%20test&expiryTime=299"
+            "https://uat.m.gcash.com/gcash-cashier-web/1.2.1/index.html#/confirm?bizNo=20250530121212800110170998402339791&timestamp=1748584909274&sign=CdMT7xkfluleR1tz26j%2F%2FqPG7xciGw8sYPBzLdEH6ybMNFgciZR24gaUxl7LAgEE8QAbe4yW%2F49Tow2%2FjIYUa16UCJcL58wf7sZVm0Ro23hSL682NpqVOXXG%2F6I68YhWM2qU90Yd16rUwBJmcDDyk9glRQomrFEp87Pf5gtEiNemWDufsut%2FGvdXUO%2FdyOOhfu8xOdpvo0YUex47ZShZpVf2JO6mJHoYmuMJXlmpOP0iu%2FvZnQqS1n6SwNAZAZ2xdPEgG7KtvQsnuj%2FKyH8RBTz2BpSxVfnGtwI5e0Jb%2FZvWOYGSia0q3noU1KQX5kNQH8jRqA7hX0CATwJnSVNdLQ%3D%3D&orderAmount=20.00&pdCode=51051000101000100001&merchantVersion=v2&merchantid=217020000648378724964&queryInterval=10000&qrcode=GCSHWPV220250530121212800110170998402339791,217020000648378724964&merchantName=Stotsenberg%20test&expiryTime=299"
           )
       );
     } else {
